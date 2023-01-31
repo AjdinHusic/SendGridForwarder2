7572b825d6ded2f8eac974bba736a0b8bc8917d6
Ajdin <ajdin@procc.nl>
2023-01-31 12:56:26 +0100

    fixed variables for docker

--- Program.cs
+++ Program.cs
@@ -1,14 +1,24 @@
+using System.Collections;
 using System.Threading.RateLimiting;
 using DotNetEnv;
+using Microsoft.Extensions.Options;
 using SendGridForwarder2;
 using SendGridForwarder2.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 Env.Load();
-var variables = Env.TraversePath().Load().ToDictionary();
+Env.TraversePath().Load();
 
 var sendGridApiKey = Env.GetString("SENDGRID_API_KEY");
+
+var variables = Environment
+    .GetEnvironmentVariables()
+    .OfType<DictionaryEntry>()
+    .Where(entry => entry.Key is string s && s.Contains("MAIL_PRESET_"))
+    .Where(entry => entry.Value is string)
+    .ToDictionary(entry => (string)entry.Key, entry => (string)entry.Value);;
+
 if (string.IsNullOrWhiteSpace(sendGridApiKey)) throw new Exception("Missing environment variable: SENDGRID_API_KEY");
 
 builder.Services.AddRateLimiter(options =>
